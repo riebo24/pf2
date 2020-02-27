b65cfc4f3f7118bb25e4733b696ef8ed5ff1ef69
rie1224 <a10ea211@yahoo.co.jp>
2020-02-27 21:19:38 +0900

    トップページのアニメーション追加

--- Gemfile
+++ Gemfile
@@ -65,3 +65,4 @@ gem 'pry-rails'
 gem 'haml-rails'
 gem 'erb2haml'
 gem 'fog-aws'   # 画像をアップデートする際、外部ストレージを選択しアップロードするのを補助してくれる
+gem 'jquery-rails'
---
+++ app/assets/javascripts/top.js
@@ -0,0 +1,7 @@
+$(function(){
+  $(".top").hover(function(){
+  $(".welcome").toggleClass("henka")
+  console.log("発火")
+  // $(".welcome").css({"animation": "fadeIn 7s ease 0s 1 normal", "visibility": "visible"})
+  })
+})
